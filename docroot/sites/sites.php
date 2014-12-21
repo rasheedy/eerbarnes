2413e6e61edb8aaa8a8356b9e32883bfc9817215
Rasheed Barnes <rasheed.barnes@acquia.com>
2014-12-21 07:15:32 -0500

    Cleaned up domains

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,11 +1,20 @@
 <?php
 
 $sites = array();
-$sites['ge-energy.com'] = 'garage';
-$sites['www.ge-energy.com'] = 'garage';
-$sites['bronxgarage.com'] = 'garage';
-$sites['bronxgarage.dev'] = 'garage';
 $sites['bronxgarage.local'] = 'garage';
-$sites['www.bronxgarage.local'] = 'garage';
-$sites['fingerpistols.dev'] = 'garage';
 $sites['fingerpistols.local'] = 'garage';
+$sites['www.bronxgarage.local'] = 'garage';
+$sites['www.bronxgarage.local'] = 'garage';
+
+$sites['bronxgarage.com'] = 'garage';
+$sites['fingerpistols.com'] = 'garage';
+$sites['www.bronxgarage.com'] = 'garage';
+$sites['www.fingerpistols.com'] = 'garage';
+
+$sites['bronxgarage.dev'] = 'garage';
+$sites['fingerpistols.dev'] = 'garage';
+$sites['www.bronxgarage.dev'] = 'garage';
+$sites['www.fingerpistols.dev'] = 'garage';
+
+
+
