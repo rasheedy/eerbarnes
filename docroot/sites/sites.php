a490064314f3c50fca0ee584a5f32ae713a410a1
Rasheed Barnes <rasheed.barnes@acquia.com>
2015-01-04 20:33:43 -0500

    Added static page for bronx garage.

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,23 +1,22 @@
 <?php
 
 $sites = array();
+$sites['ge-industry.local'] = 'rasheed';
 $sites['bronxgarage.local'] = 'garage';
 $sites['fingerpistols.local'] = 'garage';
 $sites['www.bronxgarage.local'] = 'garage';
 $sites['www.bronxgarage.local'] = 'garage';
-$sites['industry.local'] = 'garage';
-$sites['www.industry.local'] = 'garage';
+$sites['www.ge-industry.local'] = 'garage';
 
 $sites['bronxgarage.com'] = 'garage';
-$sites['fingerpistols.com'] = 'garage';
 $sites['www.bronxgarage.com'] = 'garage';
+$sites['fingerpistols.com'] = 'garage';
 $sites['www.fingerpistols.com'] = 'garage';
 
 $sites['bronxgarage.dev'] = 'garage';
 $sites['fingerpistols.dev'] = 'garage';
 $sites['www.bronxgarage.dev'] = 'garage';
 $sites['www.fingerpistols.dev'] = 'garage';
+$sites['www.ge-industry.local'] = 'garage';
 
 $sites['rasheed.local'] = 'rasheed';
-
-
---
+++ docroot/static/bronxgarage.php
@@ -0,0 +1,3 @@
+<?php  
+
+echo 'Today is '.strftime('%A, %B %d %Y', time() ).'. Check back in a week--like, Friday January 9th 2015. I should have something up.';
