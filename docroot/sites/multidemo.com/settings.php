bd745426b8af56305c5b2be802fd3250eb0e0315
Rasheed Barnes <rasheed@bronxgarage.com>
2017-02-16 10:50:38 -0500

    DB settings

--- docroot/sites/multidemo.com/settings.php
+++ docroot/sites/multidemo.com/settings.php
@@ -753,3 +753,8 @@ $settings['file_scan_ignore_directories'] = [
 // if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 //   include $app_root . '/' . $site_path . '/settings.local.php';
 // }
+
+if (file_exists('/var/www/site-php')) {
+  require('/var/www/site-php/eerbarnes/multidemo-settings.inc');
+}
+ 
